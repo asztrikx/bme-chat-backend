d9ec573fc2f90ee2211d6a394f8bb48e2f2f7222
asztrikx <asztrik35@gmail.com>
2022-11-19 22:43:01 +0100

    add privilege check

--- src/main/kotlin/com/example/message/Routing.kt
+++ src/main/kotlin/com/example/message/Routing.kt
@@ -1,21 +1,21 @@
 package com.example.message
 
+import com.example.contact.getOtherContactMember
 import com.example.database
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
-import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import org.ktorm.dsl.*
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
 
 fun Route.message() {
 	route("/message/{contactId}") {
 		get {
-			// TODO check privilege
-
+			val principal = call.principal<UserIdPrincipal>()!!
+			val userId = principal.name.toInt()
 			val contactId = call.parameters["contactId"]!!.toInt()
+
+			getOtherContactMember(userId, contactId) ?: return@get
 
 			val rows = database
 				.from(MessageSchema)
@@ -36,22 +36,6 @@ fun Route.message() {
 			}
 
 			call.respond<List<Message>>(messages)
-		}
-
-		post {
-			val principal = call.principal<UserIdPrincipal>()!!
-			val userId = principal.name.toInt()
-
-			val newMessage = call.receive<NewMessage>()
-			val datetime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yy/MM/dd HH:mm"))
-
-			database.insert(MessageSchema) {
-				set(MessageSchema.content, newMessage.content)
-				set(MessageSchema.fromUserId, userId)
-				set(MessageSchema.contactId, newMessage.contactId)
-				set(MessageSchema.toUserId, newMessage.otherUserId)
-				set(MessageSchema.date, datetime)
-			}
 		}
 	}
 }
